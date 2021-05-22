e11ad9e4dec6ec6e72017c33d54e42a73dbc3f41
nusa18 <84611120+nusa18@users.noreply.github.com>
2021-05-22 17:04:13 +0700

    Update home0259.blade.php

--- resources/views/home0259.blade.php
+++ resources/views/home0259.blade.php
@@ -4,8 +4,8 @@
 
     </head>
     <body>
-        <a href="{{url('/home0259')}}">Home</a>
-        <a href="{{url('/home/artikel0259')}}">Artikel</a>
-        <a href="{{url('/home/contact0259')}}">Contact</a>
+        <p><a href="{{url('/home0259')}}">Home</a></p>
+        <p><a href="{{url('/home/artikel0259')}}">Artikel</a></p>
+        <p><a href="{{url('/home/contact0259')}}">Contact</a></p>
     </body>
 </html>
